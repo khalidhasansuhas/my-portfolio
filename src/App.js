a9deec376538cda3993a75af299dc331d90f3bfe
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-12-07 14:13:36 +0600

    skills project data added and work component added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Skills from './components/Skills';
+import Work from './components/Work';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Home></Home>
       <About></About>
       <Skills></Skills>
+      <Work></Work>
     </div>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,58 @@
+import bikeMania from '../assets/projects/bikeMania.JPG';
+import tourGuide from '../assets/projects/tourGuide.JPG';
+import b2m from '../assets/projects/b2m.JPG';
+import octaCore from '../assets/projects/octaCore.JPG';
+import webDevQuiz from '../assets/projects/webDevQuiz.JPG';
+import webDevStudentRoutine from '../assets/projects/webDevStudentRoutine.JPG'
+export const data=[
+    {
+        id:1,
+        name:"Bike Mania",
+        image:bikeMania,
+        github:"https://github.com/khalidhasansuhas/bike-mania-client",
+        server:"https://github.com/khalidhasansuhas/bike-mania-server",
+        live:"https://voluble-crumble-66a6bc.netlify.app/",
+    },
+    {
+        id:2,
+        name:"Tour Guide",
+        image:tourGuide,
+        github:"https://github.com/khalidhasansuhas/tour-guide-client",
+        server:"https://github.com/khalidhasansuhas/tour-guide-server",
+        live:"https://frolicking-alpaca-c39df6.netlify.app/home",
+    },
+    {
+        id:3,
+        name:"B2M Learning",
+        image:b2m,
+        github:"https://github.com/khalidhasansuhas/b2m-learning-client",
+        server:"https://github.com/khalidhasansuhas/b2m-learning-server",
+        live:"https://glistening-sherbet-308d34.netlify.app/",
+    },
+    {
+        id:4,
+        name:"Web Dev Quiz",
+        image:webDevQuiz,
+        github:"https://github.com/khalidhasansuhas/web-dev-quiz",
+        server:"",
+        live:"https://curious-caramel-9bc4d6.netlify.app/home",
+    },
+    {
+        id:4,
+        name:"Web Dev Student Routien",
+        image:webDevStudentRoutine,
+        github:"https://github.com/khalidhasansuhas/web-dev-student-routine",
+        server:"",
+        live:"https://frolicking-wisp-1f95ec.netlify.app/",
+    },
+    {
+        id:4,
+        name:"Octa Core Learning",
+        image:octaCore,
+        github:"https://github.com/khalidhasansuhas/octa-core-learning",
+        server:"",
+        live:"https://frolicking-swan-6a341d.netlify.app/",
+    },
+
+
+]
