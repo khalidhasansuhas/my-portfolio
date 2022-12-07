81857c9deb7c96a298b250a37286bab9c0dbe5c3
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-12-07 14:59:21 +0600

    Contact component added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,7 +6,7 @@ const Work = () => {
     const project = data;
     //setProject(data);
     return (
-        <div name='work' className='w-full sm:h-screen text-gray-300 bg-[#0a192f]'>
+        <div name='work' className='w-full pb-16 md:h-screen text-gray-300 bg-[#0a192f]'>
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
@@ -16,7 +16,7 @@ const Work = () => {
                 </div>
 
                 {/* container for projects */}
-                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8">
 
                     {/* Gird Item */}
                     {project.map((item, index) => (
