ac6bec7c70f0fd272f71d4b5aec5078614d4cdfc
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-12-07 09:56:26 +0600

    assets and navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'>
+            <h2 className='text-red-700'>This is navbar</h2>
+        </div>
+    );
+};
+
+export default Navbar;
